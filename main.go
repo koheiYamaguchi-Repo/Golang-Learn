5baf5806ed932aa3c4fccadaa8ae6a435f5784b8
KouheiYamaguchi <messala2319@gmail.com>
2021-09-20 22:56:37 +0900

    interfaceについて

--- main.go
+++ main.go
@@ -1,57 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type intp int
-
-func (num intp) IsPrime() bool {
-	n := int(num)
-	for i := 2; i <= (n / 2); i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
+type Data interface {
+	Initial(name string, data []int)
+	PrintData()
+}
+type Mydata struct {
+	Name string
+	Data []int
 }
 
-func (num intp) PrimeFactor() []int {
-	ar := []int{}
-	x := int(num)
-	n := 2
-	for x > n {
-		if x%n == 0 {
-			x /= n
-			ar = append(ar, n)
-		} else {
-			if n == 2 {
-				n++
-			} else {
-				n += 2
-			}
-		}
-	}
-	ar = append(ar, x)
-	return ar
+func (md *Mydata) Initial(name string, data []int) {
+	md.Name = name
+	md.Data = data
 }
 
-func (num *intp) doPrime() {
-	pf := num.PrimeFactor()
-	*num = intp(pf[len(pf)-1])
+func (md *Mydata) PrintData() {
+	fmt.Println("Name: ", md.Name)
+	fmt.Println("Data: ", md.Data)
 }
 
 func main() {
-	fmt.Println("type a number")
-	var n int
-	fmt.Scanf("%d", &n)
-
-	x := intp(n)
-	fmt.Printf("%d [%t].\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
-	x.doPrime()
-	x *= 2
-	x++
-	fmt.Printf("%d [%t].\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
+	var ob Mydata = Mydata{}
+	ob.Initial("Sachiko", []int{56, 66, 77})
+	ob.PrintData()
 }
