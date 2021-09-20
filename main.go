b008598c2e2920076cb0a1e66bef238677230fff
KouheiYamaguchi <messala2319@gmail.com>
2021-09-20 17:37:12 +0900

    メソッドについて2

--- main.go
+++ main.go
@@ -13,8 +13,14 @@ func (md Mydata) PrintData() {
 }
 
 func main() {
+	taroCp := Mydata{
+		"copy", []int{20, 2020, 330},
+	}
 	taro := new(Mydata)
 	taro.Name = "hanako"
 	taro.Data = []int{30, 49, 29}
 	taro.PrintData()
+	fmt.Println(taro)
+	fmt.Println(taroCp)
+
 }
